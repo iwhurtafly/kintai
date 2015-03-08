08a60ab0d53807621d810c79e616969372fb1f88
Takashi Hayasaka <iwhurtafly@gmail.com>
2015-03-08 16:34:43 +0900

    drop table users

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308065833) do
+ActiveRecord::Schema.define(version: 20150308073134) do
 
   create_table "offices", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.integer  "office_id"
-    t.string   "user_id"
-    t.string   "name"
-    t.integer  "admin"
-    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
